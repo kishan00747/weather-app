90d077db54e4f3a0bb2b654a9d4694c256574ad5
Kishan Singh <kishan00747@gmail.com>
2025-11-05 16:45:56 +0530

    Update urls.js

--- constants/urls.js
+++ constants/urls.js
@@ -1,2 +1,2 @@
 export const LOCATION_API = 'https://ipapi.co/json/';
-export const OPEN_WEATHER_API = 'https://api.openweathermap.org/data/2.5/onecall';
+export const OPEN_WEATHER_API = 'https://api.openweathermap.org/data/2.5/forecast';
